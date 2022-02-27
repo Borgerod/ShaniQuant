71c732d1f92ece6ef373b6e8596841ceba71949f
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2022-02-27 01:06:04 +0100

    auto push

--- second_analysis/multiple_regression.py
+++ second_analysis/multiple_regression.py
@@ -9,6 +9,8 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import median_absolute_error
+import statsmodels.regression.linear_model as lm
+
 
 
 # Importing the dataset
@@ -18,18 +20,20 @@ dataset = dataset.reindex(columns=columnsTiltles)
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, 5].values
 
-# Encoding categorical data
-'''from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+'''# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, ] = labelencoder.fit_transform(X[:, ])
 onehotencoder = OneHotEncoder(categorical_features = [])
-X = onehotencoder.fit_transform(X).toarray()'''
+X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
-'''X = X[:, 1:]'''
+X = X[:, 1:]'''
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -62,17 +66,19 @@ X_opt = X[:, [0, 1, 2, 3, 4]]
 #Regressor obj from OLS class , endog : dependent variable and exog = matrix of 
 #features , as the intercept is not included, there we have to append 1
 
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS = lm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 X_opt = X[:, [1, 2, 3, 4]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS = lm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 X_opt = X[:, [1, 2, 3]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS = lm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+
 X_train1, X_test1, y_train1, y_test1 = train_test_split(X_opt, y, test_size = 0.2, random_state = 0)
 
 from sklearn.linear_model import LinearRegression
@@ -82,6 +88,7 @@ regressorr.fit(X_train1, y_train1)
 
 # Predicting the Test set results
 y_pred1 = regressorr.predict(X_test1)
+print(y_pred1)
 plt.plot(y_test, y_pred)
 plt.plot(y_test, y_pred1)
 
@@ -89,11 +96,13 @@ y_true = y_test.tolist()
 y_pred2 = y_pred1.tolist()
 
 r2_score(y_true, y_pred2)
+print(r2_score(y_true, y_pred2))
 
 mean_absolute_error(y_true, y_pred2)
-
+print(mean_absolute_error(y_true, y_pred2))
 mean_squared_error(y_true, y_pred2)
-
+print(mean_squared_error(y_true, y_pred2))
 mean_squared_log_error(y_true, y_pred2) 
-
+print(mean_squared_log_error(y_true, y_pred2))
 median_absolute_error(y_true, y_pred2)
+print(median_absolute_error(y_true, y_pred2))
